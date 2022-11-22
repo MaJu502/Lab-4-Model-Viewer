7dc801d790e1ec24c8622918384a67d50fe32d7d
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-11-22 11:31:30 -0600

    window

--- modelviewer.py
+++ modelviewer.py
@@ -1,6 +1,22 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Title: modelviewer
+
+    Description: simple 3d model viewer with textures and shaders on
+    opengl.
+
+    @author Marco Antonio Jurado 20308
+    last update: 22/11/2022
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
 import pygame
 import numpy
 import random
 from OpenGL.GL import *
 from OpenGL.GL.shaders import *
 import glm
+
+pygame.init()
+w,h = (600,600)
+pantalla = pygame.display.set_mode((w,h), pygame.OPENGL | pygame.DOUBLEBUF)
+glClearColor(0.1, 0.2, 0.3, 1)
+
